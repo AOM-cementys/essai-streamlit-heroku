c93ecaf1e8fd26d285c21d12497b32d643ee290f
AOM-cementys <75326851+AOM-cementys@users.noreply.github.com>
2020-12-30 22:26:25 +0100

    plop
    plop

---
+++ import os.py
@@ -0,0 +1,5 @@
+import os
+with open(os.path.join('C://Users//axelo//OneDrive//Documents//essai_streamlit_heroku','Procfile'), "w") as file1:
+    toFile = 'web: sh setup.sh && streamlit run <app name>.py'
+    
+file1.write(toFile)
